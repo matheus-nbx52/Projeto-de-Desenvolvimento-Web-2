d3089463c37f6e096fb6ec81e6bbd787b384036b
edsay12 <edvandearaujo2@hotmail.com>
2022-05-02 20:58:26 -0300

    Modificações + remoção de bug

--- User/src/model/UserModel.ts
+++ User/src/model/UserModel.ts
@@ -17,6 +17,7 @@ const UserSchema = new mongoose.Schema({
   userName: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
--- User/src/routes/UserRoutes.ts
+++ User/src/routes/UserRoutes.ts
@@ -1,15 +1,15 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import userController from '../controller/userController';
-
-// const bcrypt = require('bcrypt');
 
 const userRoutes = Router();
 
 // Listagem de usuarios _ (Nao deve ser mandado com o projeto ja no ar)
-userRoutes.get('/user', (req:Request, res:Response) => {
-  res.status(200).json({ status: 'tudoOk' });
-});
+userRoutes.get('/user', userController.AllUsers);
 // Vai adicionar novos usuarios
+userRoutes.post('/new', userController.newUser);
+
+userRoutes.post('/new', userController.newUser);
+
 userRoutes.post('/new', userController.newUser);
 
 export = userRoutes;
