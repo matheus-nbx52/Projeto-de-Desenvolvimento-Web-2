28629eb5518e3a02da05071bc9a771193a24bb3e
matheus-nbx52 <matheusfssilva2001@gmail.com>
2022-05-15 16:59:33 -0300

    terminado post comentario

--- Comments/src/controllers/CommentsController.ts
+++ Comments/src/controllers/CommentsController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
+import { CommentsModel } from "../models/CommentsModel";
 
 class CommentsController {
 
-    async newComment(req: Request, res: Response) { }
+    async newComment(req: Request, res: Response) { 
+        const {userID, videos_idVideos, comentario} = req.body;
+        const comments = await CommentsModel.create({
+            userID,
+            videos_idVideos,
+            comentario,
+        });
+        return res.status(201).json({          
+            error: false,
+            message: 'sucess',
+            comments,
+  });
+    }
     async findOneComment(req: Request, res: Response) { }
     async findAllComment(req: Request, res: Response) {
         res.send('opa')
