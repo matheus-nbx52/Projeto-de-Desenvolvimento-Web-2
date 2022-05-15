415c899b3e8174f3932c12f795e6ea24e9fea77e
matheus-nbx52 <matheusfssilva2001@gmail.com>
2022-05-15 20:51:57 -0300

    adicionado find all comments

--- Comments/src/controllers/CommentsController.ts
+++ Comments/src/controllers/CommentsController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { CommentsModel } from "../models/CommentsModel";
 
 class CommentsController {
-
+    //posta um comentario
     async newComment(req: Request, res: Response) { 
         const {userID, videos_idVideos, comentario} = req.body;
         const comments = await CommentsModel.create({
@@ -16,12 +16,24 @@ class CommentsController {
             comments,
   });
     }
-    async findOneComment(req: Request, res: Response) { }
-    async findAllComment(req: Request, res: Response) {
-        res.send('opa')
+    async findOneComment(req: Request, res: Response) { 
+
     }
-    async updateComment(req: Request, res: Response) { }
-    async deleteComment(req: Request, res: Response) { }
+    async findAllComment(req: Request, res: Response) {
+        const comments = await CommentsModel.findAll();
+        return comments.length > 0
+        ? res.status(200).json({
+            error: false, 
+            message: 'sucess', 
+            comments,})
+        :res.status(204).send();
+    }
+    async updateComment(req: Request, res: Response) { 
+
+    }
+    async deleteComment(req: Request, res: Response) {
+
+     }
 }
 
 export default new CommentsController();
