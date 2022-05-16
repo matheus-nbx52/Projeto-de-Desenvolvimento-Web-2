8a7e0ce6f32110068f90b6581b89d8c4ec1bffc3
matheus-nbx52 <matheusfssilva2001@gmail.com>
2022-05-16 11:15:09 -0300

    adição post video

--- Comments/src/controllers/VideoController.ts
+++ Comments/src/controllers/VideoController.ts
@@ -1,14 +1,35 @@
 import { Request, Response } from "express";
+import { VideosModel } from "../models/VideosModels";
 
 class VideoController {
-
+    //posta um novo video
     async newVideo(req: Request, res: Response) {
-        
+        const {userID, videoTitle, videoUrl, likeNumber, subject,} = req.body;
+        const videos = await VideosModel.create({
+            userID,
+            videoTitle,
+            videoUrl, 
+            likeNumber, 
+            subject,});
+            return res.status(201).json({          
+                error: false,
+                message: 'sucess',
+                videos,
+      });
+
      }
-    async findOneVideo(req: Request, res: Response) { }
-    async findAllVideos(req: Request, res: Response) { }
-    async updateVideo(req: Request, res: Response) { }
-    async deleteVideo(req: Request, res: Response) { }
+    async findOneVideo(req: Request, res: Response) {
+
+     }
+    async findAllVideos(req: Request, res: Response) {
+
+     }
+    async updateVideo(req: Request, res: Response) {
+
+     }
+    async deleteVideo(req: Request, res: Response) {
+
+     }
 }
 
 export default new VideoController();
