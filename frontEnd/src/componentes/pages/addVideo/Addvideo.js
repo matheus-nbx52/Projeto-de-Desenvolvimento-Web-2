0c37b5e9ac197b51d8c61ecf0fafc807f09033c2
edsay12 <edvandearaujo2@hotmail.com>
2022-05-16 21:57:00 -0300

    Criando rota de adiçao de videos

---
+++ frontEnd/src/componentes/pages/addVideo/Addvideo.js
@@ -0,0 +1,6 @@
+export default function AddVideo(){
+    return (
+
+        <h1>tem um frontEnd Aqui </h1>
+    )
+}
--- frontEnd/src/routes.js
+++ frontEnd/src/routes.js
@@ -5,6 +5,7 @@ import LoginPage from "./componentes/pages/login/LoginPage.js";
 import Register from "./componentes/pages/login/register.js"
 import Home from './componentes/pages/Corpo/Corpo'
 import UserPage from './componentes/pages/UserPage/UserPage.js'
+import AddVideo from "./componentes/pages/addVideo/Addvideo.js";
 
 function Rotas(){
     return(
@@ -15,6 +16,7 @@ function Rotas(){
                 <Route path="/cadastropage" exact element={<Register/>}/> 
                 <Route path="/loginpage" exact element ={<LoginPage/>}/> 
                 <Route path="/userpage" exact element ={<UserPage/>}/>
+                <Route path="/addvideo" exact element ={<AddVideo/>}/>
             </Routes>
         
         </BrowserRouter>        
