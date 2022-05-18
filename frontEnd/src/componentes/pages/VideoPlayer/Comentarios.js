0e2434c79fc2935c6d5e7b3e5d133fad63408937
edsay12 <edvandearaujo2@hotmail.com>
2022-05-18 00:03:07 -0300

    minierroReversão

--- frontEnd/src/componentes/pages/VideoPlayer/Comentarios.js
+++ frontEnd/src/componentes/pages/VideoPlayer/Comentarios.js
@@ -23,10 +23,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-<<<<<<< HEAD
+
 function Comentarios({ comentario, ItemId }) {
   const classes = useStyles()
-  
 
   return (
     <List className={classes.root}>
@@ -63,61 +62,5 @@ function Comentarios({ comentario, ItemId }) {
 
 
 
-
-
-
-    function Comentarios(){
-            const classes = useStyles()
-    
-            return(
-                <List className={classes.root}>
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-              primary="Brunch this weekend?"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="textPrimary"
-                  >
-                    Ali Connors
-                  </Typography>
-                  {" — I'll be in your neighborhood doing errands this…"}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-          <Divider variant="inset" component="li" />
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-              primary="Summer BBQ"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="textPrimary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                  {" — Wish I could come, but I'm out of town this..."}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-        </List>
-            
-            )
-    }
->>>>>>> 813eb4e9619f75d9fe371216f89bfb4578a4f158
 
 export default Comentarios
