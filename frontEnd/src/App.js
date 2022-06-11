2aded0bcce78fe3a5e360b6ddf6d9463628a5a8b
edsay12 <edvandearaujo2@hotmail.com>
2022-06-11 13:54:40 -0300

    Adicionado react redux + configs iniciais

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import './App.css'
 import Rotas from './routes'
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './store/index';
 
-export default function App(){
+export default function App() {
 
- 
-  return(
-  <>
-    <Rotas/>
-  </>
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+        />
+
+        <Rotas />
+      </PersistGate>
+
+    </Provider>
   )
 }
